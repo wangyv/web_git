e809a5617d5c2c73ace3b4ebf4ec7f9291b6477e
wangyv <1710670484@qq.com>
2017-11-03 15:46:26 +0800

    小米官网：淡入淡出轮播图

--- mi/js/mi.js
+++ mi/js/mi.js
@@ -11,41 +11,87 @@ var aDivCo=oCollocation.getElementsByTagName('div');
 var aDivAc=oAccessories.getElementsByTagName('div');
 var aDivPe=oPeriphery.getElementsByTagName('div');
 //选项卡函数
-function tabRecommend(aDiv1) {
-    var aLi;
-    var aDiv ;
-    var aDivSpanLast=new Array();
-    for(var i=0;i<aDiv1.length;i++){
-        if(aDiv1[i].className=='menu-right'){
-            aLi=aDiv1[i].getElementsByTagName('li');
-        }
-        else if(aDiv1[i].className=='container') {
-            aDiv = aDiv1[i].getElementsByTagName('div');
-            for(var i=0;i<aDiv.length;i++){
-                if(aDiv[i].className=='span-last'||aDiv[i].className=='span-last span-last-selected'){
-                    aDivSpanLast.push(aDiv[i]);
-                }
+(function (){
+    function tabRecommend(aDiv1) {
+        var aLi;
+        var aDiv ;
+        var aDivSpanLast=[];
+        for(var i=0;i<aDiv1.length;i++){
+            if(aDiv1[i].className=='menu-right'){
+                aLi=aDiv1[i].getElementsByTagName('li');
             }
-            // console.log(aDivSpanLast);
+            else if(aDiv1[i].className=='container') {
+                aDiv = aDiv1[i].getElementsByTagName('div');
+                for(var i=0;i<aDiv.length;i++){
+                    if(aDiv[i].className=='span-last'||aDiv[i].className=='span-last span-last-selected'){
+                        aDivSpanLast.push(aDiv[i]);
+                    }
+                }
+                // console.log(aDivSpanLast);
+            }
+        }
+        for(var i=0;i<aLi.length;i++){
+            aLi[i].index=i;
+            aLi[i].onmouseover=function () {
+                for(var i=0;i<aLi.length;i++){
+                    aLi[i].className='';
+                    aDivSpanLast[i].className='span-last';
+                }
+                this.className='selected';
+                aDivSpanLast[this.index].className='span-last span-last-selected';
+            };
         }
     }
+//选项卡函数调用
+    tabRecommend(aDivHa);
+    tabRecommend(aDivIn);
+    tabRecommend(aDivCo);
+    tabRecommend(aDivAc);
+    tabRecommend(aDivPe);
+})();
+
+//淡入淡出轮播图
+(function () {
+    var oLunbotuContainer=document.getElementById('lunbotu-container');
+    var oLunbotu=document.getElementById('lunbotu');
+    var aImg=oLunbotu.getElementsByTagName('img');
+    var oLunbotuTab=document.getElementById('lunbotu-tab');
+    var aLi=oLunbotuTab.getElementsByTagName('li');
+    var oLunbotuLeft=document.getElementById('lbt-left');
+    var oLunbotuRight=document.getElementById('lbt-right');
+    var iNowIndex=0;
     for(var i=0;i<aLi.length;i++){
         aLi[i].index=i;
-        aLi[i].onmouseover=function () {
-            for(var i=0;i<aLi.length;i++){
-                aLi[i].className='';
-                aDivSpanLast[i].className='span-last';
-            }
-            this.className='selected';
-            aDivSpanLast[this.index].className='span-last span-last-selected';
-        };
+        aLi[i].onclick=function () {
+            iNowIndex=this.index;
+            changeImg();
+        }
     }
-}
-//选项卡函数调用
-tabRecommend(aDivHa);
-tabRecommend(aDivIn);
-tabRecommend(aDivCo);
-tabRecommend(aDivAc);
-tabRecommend(aDivPe);
+    function changeImg() {
+        for(var i=0;i<aLi.length;i++){
+            aLi[i].className='';
+            aImg[i].className='';
+        }
+        aLi[iNowIndex].className='selected';
+        aImg[iNowIndex].className='selected';
+    }
+    oLunbotuLeft.onclick=oLunbotuRight.onclick=function () {
+        if(this===oLunbotuLeft){
+            iNowIndex--;
+            if(iNowIndex==-1){
+                iNowIndex=aLi.length-1;
+            }
+        }else{
+            iNowIndex++;
+            if(iNowIndex==aLi.length){
+                iNowIndex=0;
+            }
+        }
+        changeImg();
+    };
+    oLunbotuLeft.onselectstart=oLunbotuRight.onselectstart=oLunbotuTab.onselectstart=function () {
+        return false;
+    };
 
+})();
 
