dc992adada4b73b9d9190da6586c3ef32520a2be
wangyv <1710670484@qq.com>
2017-11-29 17:29:06 +0800

    JQ下封装一个表格排序函数

---
+++ jQuery/表格排序组件.js
@@ -0,0 +1,26 @@
+function sort(table,thidx,sortTypes) {//传入一个JQ对象，下标，排序类型
+    var $tbody = $('tbody', table);
+    var arr=[];
+    $('th',table).eq(thidx).on('click',function () {
+        var sortType=$(this).data('sorttype');
+        var sort=sortTypes;
+        var idx=$(this).index();
+        $('tr',$tbody).each(function () {
+            arr.push(this);
+        });
+        arr.sort(function(a,b){
+            var aHtml=a.children[idx].innerHTML;
+            var bHtml=b.children[idx].innerHTML;
+            if(sortType=='number'){
+                return sort=="asc"?aHtml-bHtml:bHtml-aHtml;
+            }else{
+                return sort=="asc"?aHtml.localeCompare(bHtml):bHtml.localeCompare(aHtml);
+            }
+        });
+        $(this).data('sort',$(this).data('sort')=='asc'?'desc':'asc');
+        $tbody.empty();
+        for(var i=0;i<arr.length;i++){
+            $('tbody').append(arr[i]);
+        }
+    });
+}
