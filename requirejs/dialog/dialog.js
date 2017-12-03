db8a4dda7f0f0ae19790688c20dbb55605bd05f6
wangyv <1710670484@qq.com>
2017-12-03 18:23:48 +0800

    实现弹出层的效果

---
+++ requirejs/dialog/dialog.js
@@ -0,0 +1,44 @@
+// requirejs.config({
+//     paths: {
+//         jquery: 'jquery-3.2.1'
+//     }
+// });
+define(['jquery'],function($){
+    function Dialog() {
+        this.defaultSettings={
+            width:400,
+            height:300,
+            title:'弹出层',
+            content:''
+        };
+        this.$container = $('<div class="dialog-container"></div>');
+        this.$mask = $('<div class="dialog-mask"></div>');
+        this.$box = $('<div class="dialog-box"></div>');
+        this.$title = $('<div class="dialog-title"></div>');
+        this.$item = $('<div class="dialog-item"></div>');
+        this.$close = $('<div class="dialog-close">[X]</div>');
+        this.$content = $('<div class="dialog-content"></div>');
+    }
+    Dialog.prototype.open=function (settings) {
+        $.extend(this.defaultSettings,settings);
+        this.$title.append(this.$item).append(this.$close);
+        this.$box.append(this.$title).append(this.$content).css({
+            width:this.defaultSettings.width,
+            height:this.defaultSettings.height
+        });
+        this.$container.append(this.$mask).append(this.$box).appendTo($('body'));
+        this.$item.text(this.defaultSettings.title);
+        if(this.defaultSettings.content.indexOf('.html')!=-1){
+            this.$content.load(this.defaultSettings.content);
+        }else{
+            this.$content.html(this.defaultSettings.content);
+        }
+        this.$close.on('click',function () {
+            this.close();
+        }.bind(this));
+    };
+    Dialog.prototype.close=function () {
+        this.$container.remove();
+    };
+    return Dialog;
+});
