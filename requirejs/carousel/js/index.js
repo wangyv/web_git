34bba9351f0cc4d0ae0a0e0319bdd177745c59c8
wangyv <1710670484@qq.com>
2017-12-09 16:20:26 +0800

    传入想要的 轮播图样式

---
+++ requirejs/carousel/js/index.js
@@ -0,0 +1,18 @@
+require(['jquery','carousel'],function ($,Carousel) {
+    var c1 = new Carousel();
+    var settings ={
+        imgArr:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],
+        navStyle:'circle',
+        speed:800,
+        selector:'#div1'
+    };
+    c1.init(settings);
+
+    var c2 = new Carousel();
+    var settings1 ={
+        imgArr:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],
+        speed:5000,
+        selector:'#div2'
+    };
+    c2.init(settings1);
+});
