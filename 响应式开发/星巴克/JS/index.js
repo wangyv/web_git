35062b785f48820d084ecfb2890dfd94c02bdba8
wangyv <1710670484@qq.com>
2018-01-21 20:11:26 +0800

    响应式开发 星巴克首页 基本完成 js部分

--- 响应式开发/星巴克/JS/index.js
+++ 响应式开发/星巴克/JS/index.js
@@ -17,9 +17,9 @@ $(function(){
            opacity:0,
            filter: 'alpha(opacity:0)'
        });
-        $('#nav-sub-menu').css({
+       $('#nav-sub-menu').css({
             width:0
-        });
+       });
     });
    $('#nav-sub-menu .nav-sub-menu-second li').on('click',function () {
       $(this).find('ul').slideToggle();
@@ -28,5 +28,57 @@ $(function(){
    });
    $('#nav-sub-menu .nav-sub-menu-second li a').on('click',function () {
        return false;
-   })
+   });
+
+
+   //脚部
+    $('#footer .item').on('click',function () {
+        $(this).find('.item-content').slideToggle();
+        return false;
+    });
+
+    var p = 0, t = 0;
+    //导航栏
+    $(window).on('scroll',function () {
+        p = $(this).scrollTop();
+        if(p > 100){
+            $('#nav .nav-content').add('#nav').add('#nav .nav-content div').add('#nav .nav-logo img').css({
+                height:0,
+            });
+        }else{
+            //fixed
+            $('#nav .nav-content').add('#nav').add('#nav .nav-content div').css({
+                height:'100px',
+            });
+            $('#nav .nav-logo img').css('height','73px');
+        }
+
+
+
+        if(t > p && p >100){//上滚
+            // $('#nav .nav-content').add('#nav').css({
+            //     height:'60px'
+            // });
+            // $('.nav-menu').css({
+            //     height:'53px'
+            // });
+            // $('.nav-menu img').css({
+            //     width:'34px'
+            // });
+            // $('#nav .nav-menu .nav-menu-content').css({
+            //     width:'34px',
+            //     height:'41px',
+            //     margin:'6px 10px'
+            // });
+            // $('#nav .nav-logo img').css({
+            //     height:'50px',
+            //     width:'50px'
+            // });
+            // $('#nav .nav-logo').css({
+            //     padding:'5px 0'
+            // })
+        }
+        setTimeout(function(){t = p;},0);
+    });
+
 });
