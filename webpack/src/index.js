01d3bee74d9bfd725614971b8b08a40eb40f339a
wangyv <1710670484@qq.com>
2018-01-25 10:50:37 +0800

    webpack 简单实例

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,19 @@
 import css from './css/index.css';
-
-var oDiv = document.querySelector('div');
+import sass from './css/index.scss';
+let json = require('../author.json');
+let oDiv = document.querySelector('div');
 oDiv.innerHTML = 'hello world !';
+
+class People{
+    constructor(name,age){
+        this.name = name,
+        this.age = age
+    }
+    showName(){
+        console.log(this.name);
+    }
+}
+
+let p1 = new People('zs',18);
+$('#div5').html('this is jquery!');
+$('#div6').html(`author:姓名:${json.name};年龄:${json.age}`);
