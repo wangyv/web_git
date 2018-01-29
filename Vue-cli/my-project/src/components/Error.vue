8c1209e57276b595e1e255d32480498e1ece42de
wangyv <1710670484@qq.com>
2018-01-29 22:35:00 +0800

    vue-cli rooter 基础语法 简单实例 发生错误时会进行提示的页面

---
+++ Vue-cli/my-project/src/components/Error.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>您访问的界面出去玩啦</h1>
+    <br>
+    <br>
+    <br>
+    <h1>不然换个界面试试？</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1>其实你是没网啦！！！</h1>
+</div>
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
