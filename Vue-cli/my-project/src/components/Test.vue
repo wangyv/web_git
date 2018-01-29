c3916f0573834fbb7f9975b6544db6c627a4e77a
wangyv <1710670484@qq.com>
2018-01-29 22:33:25 +0800

    vue-cli rooter 基础语法 简单实例test页面

---
+++ Vue-cli/my-project/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1 class="test">这是一个测试</h1>
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.test{
+    background-color : #ccc ;
+}
+
+</style>
