d5af2748ea7a5192b6ea48c0090e3fa063a65528
wangyv <1710670484@qq.com>
2018-01-29 22:34:04 +0800

    vue-cli rooter 基础语法 简单实例 test子路由页面

---
+++ Vue-cli/my-project/src/components/Test1.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1>这是一个子路由1</h1>
+    <h1>姓名：{{$route.params.username}}</h1>
+</div>
+
+</template>
+
+<script>
+export default {
+  beforeRouteEnter (to, from, next) {
+    console.log('准备进入路由模板')
+    next()
+  },
+  beforeRouteLeave (to, from, next) {
+    console.log('准备离开路由模板')
+    next()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
