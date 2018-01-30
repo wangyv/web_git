63177941e8c296d5f040c35e23a0388106aebb5e
wangyv <1710670484@qq.com>
2018-01-30 21:31:28 +0800

    webpack 配置文件

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,11 +3,14 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+const purifyCSSPlugin = require('purifycss-webpack');
+const webpack = require('webpack');
+const entry = require('./webpack_config/entry_webpack.js');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     //入口文件配置项
-    entry: {
-        index:'./src/index.js'
-    },
+    entry: entry,
     output: {
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js',
@@ -18,7 +21,12 @@ module.exports = {
             test:/\.css$/,
             use:ExtractTextPlugin.extract({
                 fallback:'style-loader',
-                use:'css-loader'
+                use:[{
+                    loader:'css-loader',
+                    options:{
+                        importLoaders:1
+                    }
+                },'postcss-loader']
             })
         },{
             test:/\.(png|jpg|gif)$/,
@@ -34,11 +42,32 @@ module.exports = {
             use:{
                 loader:'html-withimg-loader'
             }
+        },{
+            test:/\.scss$/,
+            use:ExtractTextPlugin.extract({
+                use:[{
+                    loader:'css-loader'
+                },{
+                    loader:'sass-loader'
+                }],
+                fallback:'style-loader'
+            })
+        },{
+            test:/\.(js|jsx)$/,
+            use:{
+                loader:'babel-loader',
+                options:{
+                    presets:[
+                        'env','react'
+                    ]
+                }
+            },
+            exclude:/node_modules/
         }]
     },
     plugins:[
         new HtmlPlugin({
-            mminify:{
+            minify:{
                 removeAttributoQuotes:true
             },
             hash:true,
@@ -46,12 +75,33 @@ module.exports = {
         }),
         new ExtractTextPlugin('css/index.css'),
         // new UglifyJsPlugin()
+        new purifyCSSPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*.html'))
+        }),
+        new webpack.BannerPlugin('翻版必究'),
+        new webpack.ProvidePlugin({
+            $:"jquery"
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:['jquery','vue'],
+            filename:'./assets/js/[name].js',
+            minChunks:2
+        }),
+        new CopyPlugin([{
+            from:__dirname + '/src/public',
+            to:__dirname + '/dist/public'
+        }])
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
         host:'localhost',
         port:'8081',
         compress:true
+    },
+    watchOptions:{
+        poll:1000,
+        aggregateTimeout:500,
+        ignored:/node_modules/,
     }
 
 }
