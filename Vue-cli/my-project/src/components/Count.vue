548284eb132b33ecc3f1c4bce11cc1d83b378f34
wangyv <1710670484@qq.com>
2018-01-30 21:37:26 +0800

    vuex 的简单例子实现简单计数器功能

---
+++ Vue-cli/my-project/src/components/Count.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{$store.state.count}}</h1>
+        <button @click="addAction('add',10)">add</button>
+        <button @click="reduceAction('reduce')">reduce</button>
+    </div>
+</template>
+
+<script>
+import store from '@/vuex/store.js'
+import {mapState, mapMutations, mapGetters, mapActions} from 'vuex'
+export default {
+  store,
+  //   computed: {
+  //     count () {
+  //       return this.$store.state.count
+  //     }
+  //   }
+  //   computed: mapState({
+  //     count: state => state.count
+  //   })
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['count'])
+  },
+  methods: {
+    ...mapMutations(['reduce']),
+    ...mapActions(['addAction', 'reduceAction'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
