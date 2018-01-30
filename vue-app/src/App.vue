3ad7e92d3b1e2960508eb45d33ab0c6ba249cb15
wangyv <1710670484@qq.com>
2018-01-30 21:39:11 +0800

    vue全家桶实例 主组件

---
+++ vue-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <common-header :bgColor="cColor" @tapped="changed">
+        <span slot="title">{{cTitle}}</span>
+    </common-header>
+    <router-view/>
+    <common-footer :bgColor="cColor" @tapped="changed"></common-footer>
+    
+  </div>
+</template>
+
+<script>
+import CommonHeader from "@/components/common/Header.vue";
+import CommonFooter from "@/components/common/Footer.vue";
+export default {
+  name: 'App',
+  data:() => {
+    return {
+      color:['rgb(33, 150, 243)','rgb(0, 150, 136)','rgb(121, 85, 72)','rgb(63, 81, 181)'],
+      title:['Movie','Music','Book','Photo'],
+      cColor:'rgb(33, 150, 243)',
+      cTitle:'Movie'
+    }
+  },
+  components: {
+    CommonHeader,
+    CommonFooter
+  },
+  methods: {
+    changed(index){
+      console.log(index)
+      this.cColor = this.color[index];
+      this.cTitle = this.title[index];
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
