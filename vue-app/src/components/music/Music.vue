84618c740094580a83c651d61e88e8c14b0904bb
wangyv <1710670484@qq.com>
2018-01-31 23:00:54 +0800

    vue全家桶实例 音乐组件

---
+++ vue-app/src/components/music/Music.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <!-- <common-header bgColor="rgb(0, 150, 136)"> -->
+        <!-- <span slot="title">Music</span> -->
+    <!-- </common-header> -->
+    <h1>movie</h1>
+    <!-- <common-footer bgColor="rgb(0, 150, 136)"></common-footer> -->
+    </div>
+
+</template>
+
+<script>
+// import CommonHeader from "@/components/common/Header.vue";
+// import CommonFooter from "@/components/common/Footer.vue";
+// export default {
+//   components: {
+//     CommonHeader,
+//     CommonFooter
+//   }
+// };
+</script>
+
+<style scoped>
+
+</style>
