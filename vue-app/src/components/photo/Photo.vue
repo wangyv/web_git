582f7d50d989d45d79be7c2880753487c3325a0d
wangyv <1710670484@qq.com>
2018-01-31 23:01:05 +0800

    vue全家桶实例  图片组件

---
+++ vue-app/src/components/photo/Photo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <!-- <common-header bgColor="rgb(63, 81, 181)"> -->
+        <!-- <span slot="title">Photo</span> -->
+    <!-- </common-header> -->
+    <h1>movie</h1>
+    <!-- <common-footer bgColor="rgb(63, 81, 181)"></common-footer> -->
+    </div>
+
+</template>
+
+<script>
+// import CommonHeader from "@/components/common/Header.vue";
+// import CommonFooter from "@/components/common/Footer.vue";
+// export default {
+//   components: {
+//     CommonHeader,
+//     CommonFooter
+//   }
+// };
+</script>
+
+<style scoped>
+
+</style>
