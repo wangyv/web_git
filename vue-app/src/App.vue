4eacc9e4c11fea2c69990ae61321cce07b0f9563
wangyv <1710670484@qq.com>
2018-01-31 22:59:02 +0800

    vue全家桶实例 主组件

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <common-header :bgColor="cColor" @tapped="changed">
-        <span slot="title">{{cTitle}}</span>
+    <common-header :bgColor="cColor" @tapped="changed('Movie')">
+        <span slot="title" v-text="cTitle"></span>
     </common-header>
-    <router-view/>
+    <router-view class="content"/>
     <common-footer :bgColor="cColor" @tapped="changed"></common-footer>
     
   </div>
@@ -13,30 +13,53 @@
 import CommonHeader from "@/components/common/Header.vue";
 import CommonFooter from "@/components/common/Footer.vue";
 export default {
-  name: 'App',
-  data:() => {
+  name: "App",
+  data: () => {
     return {
-      color:['rgb(33, 150, 243)','rgb(0, 150, 136)','rgb(121, 85, 72)','rgb(63, 81, 181)'],
-      title:['Movie','Music','Book','Photo'],
-      cColor:'rgb(33, 150, 243)',
-      cTitle:'Movie'
-    }
+      // color: [
+      //   "rgb(33, 150, 243)",
+      //   "rgb(0, 150, 136)",
+      //   "rgb(121, 85, 72)",
+      //   "rgb(63, 81, 181)"
+      // ],
+      // title: ["Movie", "Music", "Book", "Photo"],
+      cColor: "rgb(33, 150, 243)",
+      cTitle: "Movie"
+    };
   },
   components: {
     CommonHeader,
     CommonFooter
   },
   methods: {
-    changed(index){
-      console.log(index)
-      this.cColor = this.color[index];
-      this.cTitle = this.title[index];
+    changed(tagName) {
+      this.cTitle = tagName;
+      // console.log(index);
+      // this.cColor = this.color[index];
+      // this.cTitle = this.title[index];
+      switch (tagName) {
+        case "Movie":
+          this.cColor = "rgb(33, 150, 243)";
+          break;
+        case "Music":
+          this.cColor = "rgb(0, 150, 136)";
+          break;
+        case "Book":
+          this.cColor = "rgb(121, 85, 72)";
+          break;
+        case "Photo":
+          this.cColor = "rgb(63, 81, 181)";
+          break;
+        default:
+          this.cColor = "rgb(33, 150, 243)";
+      }
     }
   }
 };
-
 </script>
 
 <style>
-
+/* .content{
+  margin: 1rem 0;
+} */
 </style>
