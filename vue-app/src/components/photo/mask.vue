6249844a223bcb88c16d85a264180c7855e5ac55
wangyv <1710670484@qq.com>
2018-02-03 21:19:37 +0800

    遮罩层组件

---
+++ vue-app/src/components/photo/mask.vue
@@ -0,0 +1,31 @@
+<template id="mask">
+      <div class="mask">
+          <div class="display-img">
+            <slot name="img"></slot>
+          </div>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.mask{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    top: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 1;
+}
+.display-img{
+    position: absolute;
+    left:50%;
+    top:50%;
+    transform: translate(-50%, -50%);
+}
+</style>
