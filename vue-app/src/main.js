149c19850555dca401b0395592d9959917a4fb06
wangyv <1710670484@qq.com>
2018-02-07 18:33:47 +0800

    vue全家桶 实例 入口文件

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueTouch from 'vue-touch'
+import store from '@/vuex/store.js'
+Vue.use(VueTouch,{name: 'v-touch'})
 
 import '@/assets/js/rem.js'
 import '@/assets/css/reset.css'
+import '@/assets/js/fastclick.js'
 Vue.config.productionTip = false
 
 global.API_PROXY = 'https://bird.ioliu.cn/v2/?url='
@@ -14,6 +18,7 @@ global.API_PROXY = 'https://bird.ioliu.cn/v2/?url='
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
