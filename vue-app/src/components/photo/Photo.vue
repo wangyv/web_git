b374f59b56e9b9ec4dc88805cda5978155313320
wangyv <1710670484@qq.com>
2018-02-07 18:35:14 +0800

    vue全家桶 实例 图片部分

--- vue-app/src/components/photo/Photo.vue
+++ vue-app/src/components/photo/Photo.vue
@@ -1,12 +1,9 @@
 <template>
     <div class="container clearfix">
-        <div v-for="(obj, index) in photoList" :key = "index" class="photo-list">
-            <img :src="obj" alt="" @click="clickHandler(index)">
-        </div>
-        <div class="mask" v-show="isShow" @click="isShow = !isShow">
-          <div class="display-img">
-            <img :src="selectedImg" alt="" slot="img">
-          </div>
+        <div class="photo-list" v-for="(obj, index) in photoList" :key="index">
+          <router-link :to="'/mask/'+ index">
+            <img :src="obj.src" alt="">
+          </router-link>
         </div>
     </div>
 
@@ -17,32 +14,21 @@
 
 
 <script>
+import axios from 'axios'
+import {mapActions, mapState} from 'vuex'
+
 export default {
-  data() {
+  data(){
     return {
-      photoList: [
-        "/static/img/img/1.jpg",
-        "/static/img/img/2.jpg",
-        "/static/img/img/3.jpg",
-        "/static/img/img/4.jpg",
-        "/static/img/img/5.jpg",
-        "/static/img/img/6.jpg",
-        "/static/img/img/7.jpg",
-        "/static/img/img/8.jpg",
-        "/static/img/img/9.jpg",
-        "/static/img/img/10.jpg",
-        "/static/img/img/11.jpg",
-        "/static/img/img/12.jpg"
-      ],
-      selectedImg: "",
-      isShow: false
-    };
-  },
-  methods: {
-    clickHandler(index) {
-      this.selectedImg = this.photoList[index];
-      this.isShow = !this.isShow;
+      photoList: []
     }
+  },
+  created(){
+    axios.get('/static/data/photodata.json')
+    .then(res =>{
+      this.photoList = res.data.photoData;
+      this.$store.dispatch('getPhoto',res.data.photoData);
+    })
   }
 };
 </script>
@@ -59,28 +45,5 @@ export default {
 .photo-list {
   float: left;
   width: 50%;
-}
-.mask {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  top: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.7);
-  z-index: 1;
-}
-.display-img {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 100%;
-}
-.display-img img{
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: 90%;
 }
 </style>
