0adc46814968747cd9b117921ad8ebff3254b9f7
wangyv <1710670484@qq.com>
2018-02-12 20:56:33 +0800

    sipt 农产品溯源 控制台

---
+++ Product_traceability/src/components/Control.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <nav>
+          <img src="/static/img/control_logo.jpg" alt="">
+          <div class="menu">
+              <span class="glyphicon glyphicon-menu-hamburger"></span>
+          </div>
+          <ul>
+              <li><a href="http://">大庆市良丰农业科技有限公司</a></li>
+              <li><a href="http://">去前台</a></li>
+              <li><a href="http://">欢迎你</a></li>
+              <li>
+                <a href="http://">
+                    <span class="glyphicon glyphicon-arrow-right"></span>
+                    退出
+                </a>
+              </li>
+          </ul>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+nav{
+    height: 0.35rem;
+}
+nav img{
+    height: 0.35rem;
+    width: 0.8rem;
+    float: left;
+}
+.menu{
+    float: left;
+    font-size: 0.15rem;
+    height: 0.35rem;
+    line-height: 0.35rem;
+}
+</style>
