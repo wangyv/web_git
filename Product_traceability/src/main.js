37cfab012d216c7457c2c05dc08edf5543b5b02e
wangyv <1710670484@qq.com>
2018-02-13 23:21:04 +0800

    sipt 农产品溯源 入口文件配置

--- Product_traceability/src/main.js
+++ Product_traceability/src/main.js
@@ -5,9 +5,21 @@ import App from './App'
 import router from './router'
 import $ from 'jquery'
 import 'bootstrap/css/bootstrap.min.css'
-import 'bootstrap/js/bootstrap.min.js'
-import '@/assets/js/rem.js'
+import 'bootstrap/js/bootstrap.min'
+import '@/assets/js/rem'
 import '@/assets/css/reset.css'
+
+
+import '@/assets/css/H-ui.css'
+import '@/assets/css/H-ui.admin.css'
+import '@/assets/css/H-ui.login.css'
+import '@/assets/css/H-ui.ie.css'
+import '@/assets/Hui-iconfont/1.0.8/iconfont.css'
+import '@/assets/js/layer'
+// import '@/assets/js/H-ui.min'
+import '@/assets/js/H-ui.admin.page'
+// import '@/assets/js/index.js'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
