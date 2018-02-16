c9a117a01a0b8c84f9f5458edbd846408ac31e37
wangyv <1710670484@qq.com>
2018-02-16 08:30:12 +0800

    sipt n农产品溯源 企业证照信息，添加完善企业信息功能

--- Product_traceability/src/components/controls/BasicManagement/CertificateInfo.vue
+++ Product_traceability/src/components/controls/BasicManagement/CertificateInfo.vue
@@ -1,17 +1,24 @@
 <template>
 <!-- 企业证照信息 -->
-    <div class="license-info">
-        <div class="license-title">企业证照信息</div>
-        <hr>
-        <div class="license-content">
-            <form>
-                <div><label>企业logo图片 <small>logo</small></label><img src="/static/img/1.jpg" alt=""></div>
-                <div><label>注册商标 <small>brand</small></label><img src="/static/img/2.jpg" alt=""></div>
-                <div><label>负责人照片 <small>principle</small></label><img src="/static/img/3.jpg" alt=""></div>
-                <div><label>营业执照 <small>license</small></label><img src="/static/img/4.jpg" alt=""></div>
-            </form>
+    <div>
+        <div class="improveInfo">
+            <span>您的企业信息尚不完善？</span>
+            <router-link :to="{name:'addInfo',params:{username:'lnfy',show:false}}" class="btn btn-success add"><span class="glyphicon glyphicon-plus"></span> 去完善</router-link>
+        </div>
+        <div class="license-info">
+            <div class="license-title">企业证照信息</div>
+            <hr>
+            <div class="license-content">
+                <form>
+                    <div><label>企业logo图片 <small>logo</small></label><img src="/static/img/1.jpg" alt=""></div>
+                    <div><label>注册商标 <small>brand</small></label><img src="/static/img/2.jpg" alt=""></div>
+                    <div><label>负责人照片 <small>principle</small></label><img src="/static/img/3.jpg" alt=""></div>
+                    <div><label>营业执照 <small>license</small></label><img src="/static/img/4.jpg" alt=""></div>
+                </form>
+            </div>
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -21,6 +28,15 @@ export default {
 </script>
 
 <style scoped>
+.improveInfo{
+    text-align: right;
+    margin-right: 3%;
+    color: #ccc;
+    font-size: 0.07rem;
+}
+.improveInfo .btn{
+    border-radius: 0;
+}
 .license-info{
     width: 94%;
     margin-left: 3%;
