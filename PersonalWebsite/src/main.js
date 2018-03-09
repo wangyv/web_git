df4823ca8a14c42a190911527c7e7bdfa428f07f
wangyv <1710670484@qq.com>
2018-03-09 22:21:24 +0800

    个人网站  入口js文件

--- PersonalWebsite/src/main.js
+++ PersonalWebsite/src/main.js
@@ -5,7 +5,8 @@ import App from './App'
 import router from './router'
 import 'bootstrap/css/bootstrap.min.css'
 import 'bootstrap/js/bootstrap.min.js'
-
+import '@/assets/css/reset.css'
+import '@/assets/js/rem.js'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
