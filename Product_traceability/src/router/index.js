428bbc170d0c72932e11c76c379d8283702f5db3
wangyv <1710670484@qq.com>
2018-03-13 22:00:56 +0800

    春雨溯源系统 路由改变

--- Product_traceability/src/router/index.js
+++ Product_traceability/src/router/index.js
@@ -23,32 +23,32 @@ import ProductList from '@/components/controls/ProductManagement/ProductList'
 import CheckSession from '@/components/controls/ProductManagement/CheckSession'
 
 //工作场所及人员管理
-import StaffManagement from '@/components/controls/PersonnelManagement/StaffManagement'
-import FactoryManagement from '@/components/controls/PersonnelManagement/FactoryManagement'
-import WorkingGroupManagement from '@/components/controls/PersonnelManagement/WorkingGroupManagement'
+// import StaffManagement from '@/components/controls/PersonnelManagement/StaffManagement'
+// import FactoryManagement from '@/components/controls/PersonnelManagement/FactoryManagement'
+// import WorkingGroupManagement from '@/components/controls/PersonnelManagement/WorkingGroupManagement'
 
 //投入品管理
-import InputInventoryInformation from '@/components/controls/InputManagement/InputInventoryInformation'
-import InputUsageInformation from '@/components/controls/InputManagement/InputUsageInformation'
-import InputPurchaseInformation from '@/components/controls/InputManagement/InputPurchaseInformation'
-import InputInformationList from '@/components/controls/InputManagement/InputInformationList'
-import SupplierManagement from '@/components/controls/InputManagement/SupplierManagement'
+// import InputInventoryInformation from '@/components/controls/InputManagement/InputInventoryInformation'
+// import InputUsageInformation from '@/components/controls/InputManagement/InputUsageInformation'
+// import InputPurchaseInformation from '@/components/controls/InputManagement/InputPurchaseInformation'
+// import InputInformationList from '@/components/controls/InputManagement/InputInformationList'
+// import SupplierManagement from '@/components/controls/InputManagement/SupplierManagement'
 
 //采收管理
-import HarvestInfo from '@/components/controls/HarvestManagement/HarvestInfo'
+// import HarvestInfo from '@/components/controls/HarvestManagement/HarvestInfo'
 
 //加工管理
-import ProcessingBatch from '@/components/controls/ProcessingManagement/ProcessingBatch'
-import PackagingRecords from '@/components/controls/ProcessingManagement/PackagingRecords'
+// import ProcessingBatch from '@/components/controls/ProcessingManagement/ProcessingBatch'
+// import PackagingRecords from '@/components/controls/ProcessingManagement/PackagingRecords'
 
 //环境监测
-import EnvironmentalMonitoringData from "@/components/controls/EnvironmentalMonitoring/EnvironmentalMonitoringData"
+// import EnvironmentalMonitoringData from "@/components/controls/EnvironmentalMonitoring/EnvironmentalMonitoringData"
 
 //实施影像溯源信息采集
-import CameraList from "@/components/controls/ImageInfoCollection/CameraList"
+// import CameraList from "@/components/controls/ImageInfoCollection/CameraList"
 
 //工作环节溯源信息采集
-import InformationList from "@/components/controls/WorkSessionInfoCollection/InformationList"
+// import InformationList from "@/components/controls/WorkSessionInfoCollection/InformationList"
 
 
 
@@ -126,76 +126,6 @@ export default new Router({
         path: '/checkSession',
         name: 'checkSession',
         component: CheckSession
-      },
-      //工作场所及人员管理
-      {
-        path: '/staffManagement',
-        component: StaffManagement,
-        name: 'staffManagement'
-      },{
-        path: '/factoryManagement',
-        component: FactoryManagement,
-        name: 'factoryManagement'
-      },{
-        path: '/workingGroupManagement',
-        component: WorkingGroupManagement,
-        name: 'workingGroupManagement'
-      },
-      //投入品管理
-      {
-        path: '/inputInventoryInformation',
-        component: InputInventoryInformation,
-        name: 'inputInventoryInformation'
-      },{
-        path: '/inputUsageInformation',
-        component: InputUsageInformation,
-        name: 'inputUsageInformation'
-      },{
-        path: '/inputPurchaseInformation',
-        component: InputPurchaseInformation,
-        name: 'inputPurchaseInformation'
-      },{
-        path: '/inputInformationList',
-        component: InputInformationList,
-        name: 'inputInformationList'
-      },{
-        path: '/supplierManagement',
-        component: SupplierManagement,
-        name: 'supplierManagement'
-      },
-      //采收管理
-      {
-        path: '/harvestInfo',
-        component: HarvestInfo,
-        name: 'harvestInfo'
-      },
-      //加工管理
-      {
-        path: '/processingBatch',
-        component: ProcessingBatch,
-        name: 'processingBatch'
-      },{
-        path: '/packagingRecords',
-        component: PackagingRecords,
-        name: 'packagingRecords'
-      },
-      //环境监测
-      {
-        path: '/environmentalMonitoringData',
-        component: EnvironmentalMonitoringData,
-        name: 'environmentalMonitoringData'
-      },
-      //实施影像溯源信息采集
-      {
-        path: '/cameraList',
-        component: CameraList,
-        name: 'cameraList'
-      },
-      //工作环节溯源信息采集
-      {
-        path: '/informationList',
-        component: InformationList,
-        name: 'informationList'
       },
       ]
     }
