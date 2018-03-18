87c2ce6805bd90b39e3311939c669ad4eece9160
wangyv <1710670484@qq.com>
2018-03-18 22:05:26 +0800

    MySQL 主页，登录页

---
+++ phpStudy/MySQL/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php MySQL</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label>用户名：<input type="text" name="username"></label><br><br>
+        <label>密码：<input type="password" name="pwd"></label><br><br>
+        <label>真实姓名：<input type="text" name="realname"></label><br><br>
+        <label>电话：<input type="text" name="tel"></label><br><br>
+        <input type="submit" value="确定">
+        <input type="reset" value="重置">
+    </form>
+</body>
+</html>
