5487f6490661a8a426d3b3031a9b902e4e3e2a65
wangyv <1710670484@qq.com>
2018-03-22 22:24:53 +0800

    在router-link的to下的路由

--- Vue-cli/my-project/src/App.vue
+++ Vue-cli/my-project/src/App.vue
@@ -8,7 +8,7 @@
     <div>
         <router-link to="/">首页</router-link>
         <router-link to="/test">test</router-link>
-        <router-link to="/test2/5/lisi">test2</router-link>
+        <router-link to="/test/test2/5/lisi">test2</router-link>
         <router-link :to="{name:'/test/test1',params:{username:'zhangsan',age:18}}">test1</router-link>
         <router-link to="/home/5/lisan">重定向</router-link>
         <router-link to="/acbc">alias</router-link>
